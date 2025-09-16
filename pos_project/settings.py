d88beb5865f369bf6aea8fc17f67e827547d8652
LAB23_USER ON WS04 <L23Y15W04@LabHCI.net>
2025-09-16 16:26:18 +0800

    tech(settings): configure_settings

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -117,12 +117,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
+
